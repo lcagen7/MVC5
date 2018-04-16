d45dbf5edb8e197fb27bc76106bee61078f9136f
LCA <lca@gen7.com>
2018-04-16 20:48:43 +0530

    Files for Action Results

---
+++ MVC5/Controllers/ActionResultsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Controllers
+{
+    public class ActionResultsController : Controller
+    {
+        // GET: ActionResults
+        public ViewResult Index()
+        {
+            ViewBag.message = "This is Action Result ViewResult";
+            return View();
+        }
+
+        public PartialViewResult Partial()
+        {
+            ViewBag.message = "This is Action Result Partial";
+            if(TempData["message"] != null)
+            {
+                ViewBag.message = TempData["message"].ToString();
+            }
+            return PartialView("Index");
+        }
+
+        public RedirectResult RedirectResult()
+        {
+            TempData["message"] = "This is Redirect result.";
+            return Redirect("PartialIndex");
+        }
+
+        public RedirectToRouteResult RedirectToRoute()
+        {
+            TempData["message"] = "This is Redirect to route result.";
+            return RedirectToAction("PartialIndex");
+        }
+
+        public JsonResult JsonResult()
+        {
+            TempData["message"] = "This is Redirect to route result.";
+            return Json(new { id=1, name="One"}, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
